17ca103e32859b724fdd1a3f77ba959266c74ebd
Cedric Roger <django@trusty2>
2016-01-26 13:24:54 +0000

    first app (python manage.py startapp bets) - function-based view

--- betwit/betwit/urls.py
+++ betwit/betwit/urls.py
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, patterns, include
 from django.contrib import admin
+from bets import views
 
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-]
+admin.autodiscover()
+
+urlpatterns = patterns('',
+  url(r'^$', views.index, name='index'),
+  url(r'^admin/', include(admin.site.urls)),
+)
+#urlpatterns = [
+#    url(r'^admin/', admin.site.urls),
+#]
