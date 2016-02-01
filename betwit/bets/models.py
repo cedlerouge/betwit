a8b6807aa14742bf083b97d0dfc03171211407c1
Cedric Roger <django@trusty2>
2016-02-01 14:11:07 +0000

    create model for bets

--- betwit/bets/models.py
+++ betwit/bets/models.py
@@ -3,3 +3,24 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+from user_profile.models import User
+from matchs.models import Match
+
+class Bet(models.Model):
+    """
+    Bets model
+    """
+    user		= models.ForeignKey(User)
+    match		= models.ForeignKey(Match)
+    scoreA		= models.IntegerField()
+    scoreB		= models.IntegerField()
+    triesA		= models.IntegerField()
+    triesB		= models.IntegerField()
+    card		= models.BooleanField(default=False)
+    drop_goal		= models.BooleanField(default=False)
+    stricke     	= models.BooleanField(default=False)
+    created_date	= models.DateTimeField(auto_now_add=True)
+    modified_date	= models.DateTimeField(auto_now_add=True)
+    points_won		= models.IntegerField()
+    
