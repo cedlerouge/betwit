01fc1bed6fd1d23e4e761205e4f1540d2ad3b49c
Cedric Roger <django@trusty2>
2016-02-03 16:45:45 +0000

    customize admin interface

--- betwit/bets/admin.py
+++ betwit/bets/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
+class BetAdmin(admin.ModelAdmin):
+  list_display 	= ('user', 'match', 'created_date', 'scoreA', 'scoreB')
+  list_filter  	= ('user',)
+  ordering	= ('-created_date',)
+  search_fields	= ('match',)
 # Register your models here.
 
 from models import Bet
 
-admin.site.register(Bet)
+admin.site.register(Bet, BetAdmin)
 
