9fef57b2ff3d1ca1a0da299f8c92dcee8e5131cb
Cedric Roger <cedlerouge@dalarmor.org>
2016-02-11 10:35:42 +0000

    improve admin bets and match presentation

--- betwit/matchs/admin.py
+++ betwit/matchs/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 
+class MatchAdmin(admin.ModelAdmin):
+  list_display	= ('teamA', 'teamB', 'match_date', 'cup_round', 'scoreA', 'scoreB', 'triesA', 'triesB', 'card', 'drop_goal', 'fight')
+  list_filter   = ('teamA', 'cup_round',)
+  ordering      = ('match_date',)
+  search_fields = ('teamA',)
+
 # Register your models here.
 
 from models import Match
 
-admin.site.register(Match)
+admin.site.register(Match, MatchAdmin)
