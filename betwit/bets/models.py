8d9a161577662af3a617d700f18514114b6a2ac1
Cedric Roger <cedlerouge@dalarmor.org>
2016-02-23 22:14:52 +0000

    fix displays mistakes

--- betwit/bets/models.py
+++ betwit/bets/models.py
@@ -30,18 +30,18 @@ class BetCup(models.Model):
     BetCup - bet on final rank in the cup
     """
     teams = (
-      ('1', 'Angleterre'),
-      ('2', 'Ecosse'),
-      ('3', 'France'),
-      ('4', 'Irelande'),
-      ('5', 'Italie'),
-      ('6', 'Pays de Galles'),
+      ('an', 'Angleterre'),
+      ('ec', 'Ecosse'),
+      ('fr', 'France'),
+      ('ir', 'Irelande'),
+      ('it', 'Italie'),
+      ('pg', 'Pays de Galles'),
     )
     
 
     user		= models.ForeignKey(User)
     first		= models.CharField(max_length=15, choices=teams, default='--')
-    Second              = models.CharField(max_length=15, choices=teams, default='--')
+    second              = models.CharField(max_length=15, choices=teams, default='--')
     third               = models.CharField(max_length=15, choices=teams, default='--')
     fourth              = models.CharField(max_length=15, choices=teams, default='--')
     fifth               = models.CharField(max_length=15, choices=teams, default='--')
@@ -49,4 +49,5 @@ class BetCup(models.Model):
     grand_slam		= models.BooleanField(default=False)
     wooden_spoon	= models.BooleanField(default=False)
     points_won		= models.IntegerField(null=True, blank=True)
+    created_date	= models.DateTimeField(auto_now_add=True)
 
