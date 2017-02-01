e2619cb6342de9da40e119a277908dda8a9cd597
cedlerouge <ced@lepont.bzh>
2017-02-01 01:48:58 +0100

    tournament app: add functionnalities to add tournament/team/match

--- tournaments/models.py
+++ tournaments/models.py
@@ -10,8 +10,11 @@ class Team(models.Model):
     """
     name        = models.CharField(max_length=20)
     nationality = models.CharField(max_length=20)
-    logo        = models.ImageField(upload_to=settings.IMAGE_UPLOAD_PATH)
-    thumbnail   = models.ImageField(upload_to=settings.THUMBNAIL_UPLOAD_PATH)
+#    logo        = models.ImageField(upload_to=settings.IMAGE_UPLOAD_PATH)
+#    thumbnail   = models.ImageField(upload_to=settings.THUMBNAIL_UPLOAD_PATH)
+
+    def __str__(self):
+        return self.name
 
 class Tournament(models.Model):
     """
@@ -19,6 +22,9 @@ class Tournament(models.Model):
     """
     name        = models.CharField(max_length=20)
     year        = models.IntegerField()
+
+    def __str__(self):
+        return self.name + " - " + str(self.year)
 
 class Match(models.Model):
     """
@@ -29,26 +35,29 @@ class Match(models.Model):
         ("A","Attack")
     )
 
-    tournament_id   = models.ForeignKey(Tournament)
-    date            = models.DateTimeField(auto_now_add=False)
+    tournament_id   = models.ForeignKey( Tournament )
+    date            = models.DateTimeField( auto_now_add=False )
     #tournament_round = models.IntegerField()
-    home_team_id    = models.IntegerField()
-    home_team_score = models.IntegerField()
-    home_team_tries = models.IntegerField()
-    home_team_bonus = models.CharField(max_length=15, choices=bonus, default='--')
-    home_team_point = models.IntegerField() 
-    away_team_id    = models.IntegerField()
-    away_team_score = models.IntegerField()
-    away_team_tries = models.IntegerField() 
-    away_team_bonus = models.CharField(max_length=15, choices=bonus, default='--')    
-    away_team_point = models.IntegerField() 
-    card            = models.BooleanField(default=False)
-    drop_goal       = models.BooleanField(default=False)
-    fight           = models.BooleanField(default=False)
-    created_date    = models.DateTimeField(auto_now_add=True)
-    modified_date   = models.DateTimeField(auto_now_add=True)
-    points_won      = models.IntegerField(null=True, blank=True, default=0)
-    comment         = models.CharField( max_length=1000 )
+    home_team_id    = models.ForeignKey(Team, related_name="home_team" )
+    home_team_score = models.IntegerField( null=True, blank=True )
+    home_team_tries = models.IntegerField( null=True, blank=True )
+    home_team_bonus = models.CharField( max_length=15, choices=bonus, default='--' )
+    home_team_point = models.IntegerField( null=True, blank=True ) 
+    away_team_id    = models.ForeignKey( Team, related_name="away_team" )
+    away_team_score = models.IntegerField( null=True, blank=True )
+    away_team_tries = models.IntegerField( null=True, blank=True ) 
+    away_team_bonus = models.CharField( max_length=15, choices=bonus, default='--' )    
+    away_team_point = models.IntegerField( null=True, blank=True ) 
+    card            = models.BooleanField( default=False )
+    drop_goal       = models.BooleanField( default=False )
+    fight           = models.BooleanField( default=False )
+    created_date    = models.DateTimeField( auto_now_add=True )
+    modified_date   = models.DateTimeField( auto_now_add=True )
+    points_won      = models.IntegerField( null=True, blank=True, default=0 )
+    comment         = models.TextField( max_length=1000, null=True, blank=True )
+
+    def __unicode__(self):
+        return "%s > %s" % (self.home_team_id, self.away_team_id)
 
 class MatchPoint(models.Model):                     
     """                                           
--- tournaments/urls.py
+++ tournaments/urls.py
@@ -3,11 +3,13 @@ from . import views
 
 urlpatterns = [
     url( r'^$', views.tournament_list, name='tournament_list' ),
-    url( r'^tournament/add/$', views.tournament_form, name='tournament_form' ),
-    url( r'^tournament/(?P<tournament_id>[0-9]+)/$', views.match_list_by_tournament, name = "match_list_by_tournament" ),
-    url( r'^tournament/(?P<tournament_id>[0-9]+)/match_add/$', views.match_form, name='match_form' ),
+    url( r'^tournament/add/$', views.tournament_add, name='tournament_add' ),
+    url( r'^tournament/(?P<tournament_id>[0-9]+)/$', views.tournament_detail, name = "tournament_detail" ),
+    url( r'^tournament/(?P<tournament_id>[0-9]+)/match_add/$', views.match_add, name='match_add' ),
+    # TODO url( r'^tournament/(?P<tournament_id>[0-9]+)/team_add/$', views.team_add_to, name='team_add_to' ),
     url( r'^team/$', views.team_list, name="team_list" ),
-    url( r'^team/add/$', views.team_form, name='team_form' ),
+    url( r'^team/add/$', views.team_add, name='team_add' ),
+    #url( r'^team/team_add/$', views.team_add, name='team_add' ),
     url( r'^team/(?P<team_id>[0-9]+)/$', views.team_detail, name = "team_detail" ),
     url( r'^match/$', views.match_list, name='match_list' ),
     url( r'^match/(?P<match_id>[0-9]+)/$', views.match_detail, name = "match_detail" ),
