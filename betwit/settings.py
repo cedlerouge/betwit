7130143ced7b085a422a45373f4c902f4b00654e
cedlerouge <ced@lepont.bzh>
2017-02-03 08:14:35 +0100

    added loggin functionnality. logging in console

--- betwit/settings.py
+++ betwit/settings.py
@@ -106,6 +106,22 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+        },
+    },
+}
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
