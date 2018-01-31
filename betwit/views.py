2395ce4784616884a020510356d781fb5f4da7b2
cedlerouge <ced@lepont.bzh>
2018-01-31 23:15:54 +0100

    [ADD] list blog entries on home page

--- betwit/views.py
+++ betwit/views.py
@@ -4,12 +4,30 @@ from django.db.models import Count
 from django.shortcuts import render
 from django.views.generic import View
 
+from tournaments.models import Tournament
+from andablog.models import Entry
+import tournaments.board
+import datetime
+from django.db.models import Q
 
 
 class HomeView(View):
     template_name = 'home.html'
 
     def get(self, request):
-         return render(request, self.template_name, {
-            
-        })
+
+        content = {
+
+            # Blog
+            'entries':  Entry.objects.filter(Q(is_published=True) | Q(author__isnull=False))
+            #comments = Comment.objects.last(5)
+
+            # tournament
+            #tid = Tournament.objects.filter(state=1,year=datetime.now().year)
+            #results = tournaments.board.getTeamTable(tid)
+
+
+
+        }
+
+        return render(request, self.template_name, { 'content': content })
