1b8e9820c599f267fe56e8a07f3d3ebab103afe2
cedlerouge <ced@lepont.bzh>
2018-02-04 15:26:57 +0100

    [FIX] display bet detail for a match : define an offset on srolltop

--- betwit/static/js/betwit.js
+++ betwit/static/js/betwit.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $(".toggler").click(function(e){
         e.preventDefault();
         $('.round'+$(this).attr('data-betwit-matchid')).toggle(1000);
-        $('html, body').animate({ scrollTop: $("#"+$(this).attr('data-betwit-matchid')).offset().top}, 1000);
+        $('html, body').animate({ scrollTop: $("#"+$(this).attr('data-betwit-matchid')).offset().top-51}, 1000);
     });
 });
 
