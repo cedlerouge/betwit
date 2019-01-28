5575da7077132df7c653d65d90ddedcb64d7ec93
cedlerouge <ced@lepont.bzh>
2019-01-28 17:25:54 +0100

    bug in tournament bet that raised error about string is not Team object when adding new tournament bet. This is done by creating a new form field TeamListField

--- bets/forms.py
+++ bets/forms.py
@@ -1,6 +1,7 @@
 from django.forms import ModelForm, Textarea
 from django import forms
 from bets.models import MatchBet,TournamentBet, BetPoint, Profile
+from tournaments.models import Team
 from django.contrib.auth.models import User
 from tournaments.models import Match
 from django.utils import timezone
@@ -33,37 +34,33 @@ class MatchBetForm( ModelForm ):
         model   = MatchBet
         exclude = ['player', 'points_won']
 
+class TeamListField(forms.ChoiceField):
+    teams = [(None, '-- choose a team --'), ]
+    #teams   = [(None, '-- choose a team --'), ]
+    def __init__(self, *args, **kwargs):
+        tid = kwargs.pop('tournament', None)
+        super(TeamListField, self).__init__(*args, **kwargs)
+        if tid is not None:
+            matchs  = Match.objects.filter( tournament = tid )
+            for m in matchs:
+                t = (m.home_team.name, m.home_team.name)
+                if t not in self.teams:
+                    self.teams.append(t)
+                t = (m.away_team.name, m.away_team.name )
+                if t not in self.teams:
+                   self.teams.append(t)
+        self.choices = self.teams
+        #self.widget = forms.Select()
+        #self.help_text = ""
+    
+    def to_python(self, value):
+        return Team.objects.get(name = value)
+        
 class TournamentBetForm( ModelForm ):
     """
     TournamentBet form which will be closed at the begining of the first match
     """
-#    teams   = [(None, '-- choose a team --'), ]
-#    first_team  = forms.ChoiceField(
-#                choices = teams,
-#                initial = (None, '-- choose a match --'),
-#                required= True )
-#    second_team  = forms.ChoiceField(
-#                choices = teams,
-#                initial = (None, '-- choose a match --'),
-#                required= True )
-#    third_team  = forms.ChoiceField(
-#                choices = teams,
-#                initial = (None, '-- choose a match --'),
-#                required= True )
-#    fourth_team  = forms.ChoiceField(
-#                choices = teams,
-#                initial = (None, '-- choose a match --'),
-#                required= True )
-#    fifth_team  = forms.ChoiceField(
-#                choices = teams,
-#                initial = (None, '-- choose a match --'),
-#                required= True )
-#    sixth_team  = forms.ChoiceField(
-#                choices = teams,
-#                initial = (None, '-- choose a match --'),
-#                required= True )
-
-    # TODO get every team name of the tournament
+    #TODO get every team name of the tournament
     def __init__( self, *args, **kwargs):
         tid     = kwargs.pop( 'tournament', None)
         teams   = [(None, '-- choose a team --'), ]
@@ -78,30 +75,12 @@ class TournamentBetForm( ModelForm ):
                    teams.append(t)
 
         super(TournamentBetForm, self).__init__(*args, **kwargs)
-        self.fields['first_team'] = forms.ChoiceField(
-                                        choices = teams,
-                                        widget  = forms.Select(),
-                                        help_text = "" )
-        self.fields['second_team'] = forms.ChoiceField(
-                                        choices = teams,
-                                        widget  = forms.Select(),
-                                        help_text = "" )
-        self.fields['third_team'] = forms.ChoiceField(
-                                        choices = teams,
-                                        widget  = forms.Select(),
-                                        help_text = "" )
-        self.fields['fourth_team'] = forms.ChoiceField(
-                                        choices = teams,
-                                        widget  = forms.Select(),
-                                        help_text = "" )
-        self.fields['fifth_team'] = forms.ChoiceField(
-                                        choices = teams,
-                                        widget  = forms.Select(),
-                                        help_text = "" )
-        self.fields['sixth_team'] = forms.ChoiceField(
-                                        choices = teams,
-                                        widget  = forms.Select(),
-                                        help_text = "" )
+        self.fields['first_team'] = TeamListField( tournament = tid)
+        self.fields['second_team'] = TeamListField( tournament = tid)
+        self.fields['third_team'] = TeamListField( tournament = tid)
+        self.fields['fourth_team'] = TeamListField( tournament = tid)
+        self.fields['fifth_team'] = TeamListField( tournament = tid)
+        self.fields['sixth_team'] = TeamListField( tournament = tid)
 
 
     class Meta:
